a040885678fa73f2d689b387802ea685c4edd83e
DJ1 <jimmynoble604@gmail.com>
2023-05-17 22:39:08 -0400

    finished and working.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
       <nav>
         <Link to="/">Home</Link>
         <Link to="/pizza">Order</Link>
--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -2,43 +2,113 @@
 /* eslint-disable no-undef */
 
 import axios from "axios";
+import { useState } from  "react";
+import * as yup from "yup";
+import { formSchema } from "./formSchema";
+
 const initialFormState = {
-    name: '',
-    size: '',
-    hasMushrooms: false,
+    name: "",
+    size: "",
+    hasMushroom: false,
     hasPepperoni: false,
     hasBananaPeppers: false,
     hasExtraCheese: false,
-    specialText: ''
-}
+    specialText: "",
+};
+const initialErrorState = {
+    name: "",
+};
 export const Pizza = (props) => {
     const [formValues, setFormValues] = useState(initialFormState);
+    const [errors, setErrors] = useState(initialErrorState);
     const handleChange = (e) => {
-        console.log(e.target.name);
-    }
+        if (e.target.type === "checkbox") {
+            setFormValues({
+                ...formValues,
+                [e.target.name]: !formValues[e.target.name],
+            });
+        } else {
+            setFormValues({ ...formValues, [e.target.name]: e.target.value });
+        }
+        if (e.target.name === "name") {
+            yup
+            .reach(formSchema, e.target.name)
+            .validate(e.target.value)
+            .then((valid) => {
+                console.log('no error');
+                setErrors({
+                    ...errors,
+                    [e.target.name]: "",
+                });
+            })
+            .catch((err) => {
+                console.log(err.errors[0]);
+                setErrors({
+                    ...errors,
+                    [e.target.name]: err.errors[0],
+                });
+            });
+        }
+    };
     const onSubmitCLicked = (e) => {
         e.preventDefault();
-        axios.post('https://reqres.in/api/orders', {}).then(res => {
+        axios.post('https://reqres.in/api/orders', {}).then((res) => {
             console.log(res);
-        })
-    }
+        });
+    };
     return (
         <div>
             <form onSubmit={onSubmitCLicked} id="pizza-form">
-                <input name="name" onChange={handleChange} value={formValues.name} id="name-input" />
-                <select name="size" onChange={handleChange} value={formValues.size} id="size-dropdown">
+                <input 
+                name="name" 
+                onChange={handleChange} 
+                value={formValues.name} 
+                id="name-input" 
+                />
+                {errors.name && <p>{errors.name}</p>}
+                <select 
+                name="size" 
+                onChange={handleChange} 
+                value={formValues.size} 
+                id="size-dropdown"
+                >
                     <option value=""></option>
                     <option value="sm">Small</option>
                     <option value="md">Medium</option>
                     <option value="lg">Large</option>
                 </select>
-                <input name="hasMushroom" onChange={handleChange} checked={formValues.hasMushrooms} type="checkbox" />Mushrooms
-                <input name="hasPepperoni" onChange={handleChange} checked={formValues.hasPepperoni} type="checkbox" />Pepperoni
-                <input name="hasBananaPeppers" onChange={handleChange} checked={formValues.hasBananaPeppers} type="checkbox" />Banana Peppers
-                <input name="hasExtraCheese" onChange={handleChange} checked={formValues.hasExtraCheese} type="checkbox" />Extra Cheese
-                <input name="hasSpecialText" onChange={handleChange} value={formValues.specialText} id="special-text" />
+                <input 
+                name="hasMushroom" 
+                onChange={handleChange} 
+                checked={formValues.hasMushroom} 
+                type="checkbox" 
+                />Mushroom
+                <input 
+                name="hasPepperoni" 
+                onChange={handleChange} 
+                checked={formValues.hasPepperoni} 
+                type="checkbox" 
+                />Pepperoni
+                <input 
+                name="hasBananaPeppers" 
+                onChange={handleChange} 
+                checked={formValues.hasBananaPeppers} 
+                type="checkbox" 
+                />Banana Peppers
+                <input 
+                name="hasExtraCheese" 
+                onChange={handleChange} 
+                checked={formValues.hasExtraCheese} 
+                type="checkbox" 
+                />Extra Cheese
+                <input 
+                name="specialText" 
+                onChange={handleChange} 
+                value={formValues.specialText} 
+                id="special-text" 
+                />
                 <button id="order-button">Order</button>
             </form>
         </div>
-    )
-}
+    );
+};
